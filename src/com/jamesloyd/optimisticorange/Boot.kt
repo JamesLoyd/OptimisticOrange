be642de7ef44a1aa6c48cba316f2cc4c67575bbb
James Loyd <jamestloyd@gmail.com>
2019-05-16 18:40:13 -0400

    Interfaces can define default bodies....

--- src/com/jamesloyd/optimisticorange/Boot.kt
+++ src/com/jamesloyd/optimisticorange/Boot.kt
@@ -2,6 +2,7 @@ package com.jamesloyd.optimisticorange
 
 fun main(){
     Boot().showAHello()
+    Test.doTheThing()
 }
 
 class Boot {
@@ -9,3 +10,11 @@ class Boot {
         println("Hello world I am James Loyd")
     }
 }
+
+interface IThingDoer{
+    fun doTheThing(){
+        println("So interfaces can define method bodys...")
+    }
+}
+
+object Test : IThingDoer
